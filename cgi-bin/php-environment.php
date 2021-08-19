ddf07170ce55a7b7214f150a6424a8d023419536
MuhammadCF <ochafajar@gmail.com>
2021-08-19 06:33:59 -0700

    php env

---
+++ cgi-bin/php-environment.php
@@ -0,0 +1,22 @@
+<?php
+
+
+// $addr = $_SERVER['REMOTE_ADDR'];
+
+// echo "Cache-Control: no-cache\n";
+// echo "Content-type: text/html\n\n";
+header("Cache-Control: no-cache");
+header("Content-Type: text/html");
+echo <<<EOL
+<html><head><title>Environment Variables</title></head>
+<body><h1 align=center>Environment Variables</h1>
+<hr/>\n
+EOL;
+echo "Environment Variables<br/>\n";
+foreach ($_SERVER as $var => $val){
+    echo "$var = $val\n";
+}
+
+echo "</body></html>";
+
+?>
