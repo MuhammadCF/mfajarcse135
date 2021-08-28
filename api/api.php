e89a181107347ccfa25b2e38d0c2445b987138bd
MuhammadCF <ochafajar@gmail.com>
2021-08-28 08:01:46 -0700

    adding  first get route

--- api/api.php
+++ api/api.php
@@ -2,31 +2,53 @@
 // Include router class
 include('Route.php');
 
-// Add base route (startpage)
-Route::add('/',function(){
-    echo 'Welcome :-)';
-});
+// // Add base route (startpage)
+// Route::add('/',function(){
+//     echo 'Welcome :-)';
+// });
 
-// Simple test route that simulates static html file
-Route::add('/test.html',function(){
-    echo 'Hello from test.html';
-});
+// // Simple test route that simulates static html file
+// Route::add('/test.html',function(){
+//     echo 'Hello from test.html';
+// });
+
+// // Post route example
+// Route::add('/contact-form',function(){
+//     echo '<form method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
+// },'get');
+
+// // Post route example
+// Route::add('/contact-form',function(){
+//     echo 'Hey! The form has been sent:<br/>';
+//     print_r($_POST);
+// },'post');
+
+// // Accept only numbers as parameter. Other characters will result in a 404 error
+// Route::add('/foo/([0-9]*)/bar',function($var1){
+//     echo $var1.' is a great number!';
+// });
+
 
 // Post route example
-Route::add('/contact-form',function(){
-    echo '<form method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
+Route::add('/static/',function(){
+    echo <<<EOL
+
+    static: {
+        userAgent: null,
+        language: null,
+        acceptsCookies: null,
+        screenDimensions: {
+          inner: {
+            innerWidth: null,
+            innerHeight: null
+          },
+          outer: {
+            outerWidth: null,
+            outerHeight: null
+          }
+        }
+    EOL;
 },'get');
-
-// Post route example
-Route::add('/contact-form',function(){
-    echo 'Hey! The form has been sent:<br/>';
-    print_r($_POST);
-},'post');
-
-// Accept only numbers as parameter. Other characters will result in a 404 error
-Route::add('/foo/([0-9]*)/bar',function($var1){
-    echo $var1.' is a great number!';
-});
 
 Route::run('/');
 
