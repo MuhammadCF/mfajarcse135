2a84a29b46cf3dbfb4fdfcb016ed1ba541ce0aa4
MuhammadCF <ochafajar@gmail.com>
2021-08-28 18:48:00 -0700

    testingsessionStorage 6

--- sendData.js
+++ sendData.js
@@ -6,10 +6,14 @@ function sendInit(){
     let store = window.sessionStorage;
 
     store.setItem('static', JSON.stringify(data.static));
+    
+    setTimeout(function (){
+        store.setItem('activity', JSON.stringify(data.activity));
+    }, 5000);
+
     setTimeout(function (){
         store.setItem('performance', JSON.stringify(data.performance));
     }, 1000);
-    store.setItem('activity', JSON.stringify(data.activity));
     
 }
 
